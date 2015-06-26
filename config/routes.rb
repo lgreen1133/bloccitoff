36b5b211d01d5570608a4825f28ca21991f96a53
Liz <lizelotte.green@gmail.com>
2015-06-26 16:06:53 -0500

    Added Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+    resources :users, only: [:show]
   devise_scope :user do
     authenticated :user do 
       root to: 'devise/registrations#edit', as: :authenticated
