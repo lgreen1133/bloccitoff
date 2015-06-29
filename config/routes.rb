ff08030c3ba903604fd412a5350f0d59fe9b6bcb
Liz <lizelotte.green@gmail.com>
2015-06-29 16:00:19 -0500

    Created item model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
     resources :users, only: [:show]
+      resources :items, only: [:create]
   devise_scope :user do
     authenticated :user do 
       root to: 'users#show', as: :authenticated
