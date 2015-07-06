d4e771833f11aa4a0ccbc3cbcf17e5fd3d855fcf
Liz <lizelotte.green@gmail.com>
2015-07-06 14:57:22 -0500

    Display Time Remaining for listed items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+
+  default_scope { order('updated_at DESC') }
+
+  def days_left
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
+  end
+
+  def days_left_text
+  "#{self.days_left} days left"
+  end
 end
