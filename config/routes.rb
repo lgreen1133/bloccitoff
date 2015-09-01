ae5a13ed8902030e11c799ed6150dae7d382784d
Liz <lizelotte.green@gmail.com>
2015-08-31 21:31:47 -0500

    Added welcome page, changed theme, moved things around

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
     resources :users, only: [:show]
       resources :items, only: [:create, :destroy]
-  devise_scope :user do
-    authenticated :user do 
-      root to: 'users#show', as: :authenticated
-    end
 
-    unauthenticated do 
-    root to: 'devise/sessions#new', as: :unauthenticated
-    end
-  end
+  root to: 'welcome#index'
 end
