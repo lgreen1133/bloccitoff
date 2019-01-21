5c767679c5971d2c5265a78b8c0eb66c2bb93f48
Liz <lizelotte.green@gmail.com>
2019-01-20 18:33:42 -0600

    Maintenance: updated devise convention, gems, and swapped cancel for delete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up)
+    devise_parameter_sanitizer.permit(:account_update)
   end
 end
